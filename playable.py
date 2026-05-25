0fca9750c7f9b945d4822daf065832adae18dba0
kevin.kupp <kevin.kupp@voco.ee>
2026-05-25 11:02:10 +0300

    Updates

--- playable.py
+++ playable.py
@@ -3,6 +3,25 @@ import sys
 import random
 
 # --- ERALDI SKOORI KLASS ---
+class Timer:
+    def __init__(self):
+        self.algusaeg = pygame.time.get_ticks()
+        self.font = pygame.font.SysFont("Arial", 24, bold=True)
+        self.sekundid = 0
+
+    def uuenda(self):
+        # Arvutame möödunud aja sekundites
+        self.sekundid = (pygame.time.get_ticks() - self.algusaeg) // 1000
+
+    def joonista(self, aken):
+        tekst = self.font.render(f"Aeg: {self.sekundid}s", True, (255, 255, 255))
+        # Joonistame skoorist veidi allapoole
+        aken.blit(tekst, (20, 60))
+
+    def reset(self):
+        self.algusaeg = pygame.time.get_ticks()
+        self.sekundid = 0
+
 class Score:
     def __init__(self):
         self.points = 0
@@ -12,9 +31,7 @@ class Score:
         self.points += kogus
 
     def joonista(self, aken):
-        # Tekitame tekstist pildi (render)
         skoor_pind = self.font.render(f"Score: {int(self.points)}", True, (255, 255, 255))
-        # Joonistame selle ekraanile (blit) - asukoht 20, 20 on ekraanil nähtav
         aken.blit(skoor_pind, (20, 20))
 
     def reset(self):
@@ -24,6 +41,9 @@ class Car:
     def __init__(self, x, y):
         self.rect = pygame.Rect(x, y, 50, 90)
         self.kiirus = 7
+        self.PUNANE = (191, 12, 12)
+        self.MUST = (30, 30, 30)
+        self.KLAAS = (173, 216, 230) # Helesinine
 
     def liiguta(self):
         klahvid = pygame.key.get_pressed()
@@ -32,9 +52,31 @@ class Car:
         if klahvid[pygame.K_RIGHT] and self.rect.right < 640:
             self.rect.x += self.kiirus
 
-    def joonista(self, aken):
-        pygame.draw.rect(aken, (255, 0, 0), self.rect)
-        pygame.draw.rect(aken, (0, 0, 0), self.rect, 2)
+    def joonista(self, aken, varv=(191, 12, 12)):
+        # 1. Kere (põhivärv)
+        pygame.draw.rect(aken, varv, self.rect, border_radius=8)
+        # Kere piirjoon
+        pygame.draw.rect(aken, (30, 30, 30), self.rect, 2, border_radius=8)
+
+        # 2. Rattad (4 musta ristkülikut nurkades)
+        ratta_laius = 10
+        ratta_korgus = 18
+        # Vasak ees, Parem ees, Vasak taga, Parem taga
+        rattad = [
+            (self.rect.x - 2, self.rect.y + 10),
+            (self.rect.right - ratta_laius + 2, self.rect.y + 10),
+            (self.rect.x - 2, self.rect.bottom - 25),
+            (self.rect.right - ratta_laius + 2, self.rect.bottom - 25)
+        ]
+        for r in rattad:
+            pygame.draw.rect(aken, (30, 30, 30), (r[0], r[1], ratta_laius, ratta_korgus), border_radius=3)
+
+        # 3. Klaasid (Helesinine)
+        klaasi_varv = (173, 216, 230)
+        # Esiklaas
+        pygame.draw.rect(aken, klaasi_varv, (self.rect.x + 8, self.rect.y + 20, 34, 15), border_radius=2)
+        # Tagaklaas
+        pygame.draw.rect(aken, klaasi_varv, (self.rect.x + 8, self.rect.bottom - 30, 34, 10), border_radius=2)
 
 class McTrip:
     def __init__(self):
@@ -59,11 +101,11 @@ class McTrip:
         self.triibud = [[315, i * self.triibu_vahe] for i in range(10)]
         self.auto = Car(300, 800)
         
-        # LOOME SKOORI OBJEKTI
         self.skoor = Score()
-        
         self.vastased = []
+        self.vastaste_kiirus = 10 
         self.teksti_timer = 180
+        self.taimer = Timer()
 
     def reset_mang(self):
         self.vastased = []
@@ -71,7 +113,26 @@ class McTrip:
         self.mang_kaib = True
         self.mang_labi = False
         self.teksti_timer = 180
-        self.skoor.reset() # Nullime skoori uue mängu alguses
+        self.vastaste_kiirus = 10 
+        self.skoor.reset()
+        self.taimer.reset()
+    
+    def joonista_auto_mudel(self, aken, rect, varv):
+        # Kere
+        pygame.draw.rect(aken, varv, rect, border_radius=8)
+        pygame.draw.rect(aken, (30, 30, 30), rect, 2, border_radius=8)
+        
+        # Rattad
+        ratta_v = (30, 30, 30)
+        pygame.draw.rect(aken, ratta_v, (rect.x - 2, rect.y + 10, 10, 18), border_radius=3)
+        pygame.draw.rect(aken, ratta_v, (rect.right - 8, rect.y + 10, 10, 18), border_radius=3)
+        pygame.draw.rect(aken, ratta_v, (rect.x - 2, rect.bottom - 25, 10, 18), border_radius=3)
+        pygame.draw.rect(aken, ratta_v, (rect.right - 8, rect.bottom - 25, 10, 18), border_radius=3)
+
+        # Klaasid
+        klaas = (173, 216, 230)
+        pygame.draw.rect(aken, klaas, (rect.x + 8, rect.y + 20, 34, 15), border_radius=2)
+        pygame.draw.rect(aken, klaas, (rect.x + 8, rect.bottom - 30, 34, 10), border_radius=2)
 
     def tekita_vastane(self):
         if random.random() < 0.04:
@@ -107,7 +168,6 @@ class McTrip:
             pygame.draw.rect(self.aken, self.VALGE, (triip[0], triip[1], 10, 40))
 
         if not self.mang_kaib and not self.mang_labi:
-            # Algusmenüü
             pygame.draw.rect(self.aken, self.VALGE, self.nupp_rect)
             pygame.draw.rect(self.aken, self.MUST, self.nupp_rect, 5)
             font = pygame.font.SysFont("Calibri", 60)
@@ -115,10 +175,11 @@ class McTrip:
             self.aken.blit(tekst, (self.nupp_rect.x + 30, self.nupp_rect.y + 10))
 
         elif self.mang_labi:
-            # Kaotuse ekraan
             font = pygame.font.SysFont("Calibri", 40)
             fail_tekst = font.render(f"Final Score: {int(self.skoor.points)}", True, self.VALGE)
+            fail_text = font.render(f"Final Time: {int(self.taimer.sekundid)}", True, self.VALGE)
             self.aken.blit(fail_tekst, (220, 350))
+            self.aken.blit(fail_text, (220, 410))
             
             pygame.draw.rect(self.aken, self.VALGE, self.reset_nupp_rect)
             pygame.draw.rect(self.aken, self.MUST, self.reset_nupp_rect, 5)
@@ -127,14 +188,14 @@ class McTrip:
             self.aken.blit(btn_tekst, (self.reset_nupp_rect.x + 25, self.reset_nupp_rect.y + 15))
 
         elif self.mang_kaib:
-            self.auto.joonista(self.aken)
+            self.auto.joonista(self.aken, (191, 12, 12)) 
+
             for v_andmed in self.vastased:
                 rect, varv = v_andmed
-                pygame.draw.rect(self.aken, varv, rect)
-                pygame.draw.rect(self.aken, self.MUST, rect, 2)
+                self.joonista_auto_mudel(self.aken, rect, varv)
 
-            # SKOORI JOONISTAMINE
             self.skoor.joonista(self.aken)
+            self.taimer.joonista(self.aken)
 
             if self.teksti_timer > 0:
                 font = pygame.font.SysFont("Calibri", 32)
@@ -144,7 +205,7 @@ class McTrip:
                 self.aken.blit(tekst_pind, (210, 200))
 
         pygame.display.flip()
-
+    
     def run(self):
         while True:
             for sündmus in pygame.event.get():
@@ -161,24 +222,36 @@ class McTrip:
                             self.reset_mang()
             
             self.uuenda_tausta()
-            
+                 
             if self.mang_kaib:
-                # SKOORI LISAMINE: 2 punkti sekundis (60 FPS juures)
+                # Skoori lisamine
                 self.skoor.lisa_punkte(2 / 60)
+                self.taimer.uuenda()
                 
+                # KONTROLL: Kas skoor on 10 täis?
+                if self.skoor.points >= 300:
+                    pygame.quit()
+                    sys.exit()
+                        
+                # Taseme ja kiiruse arvutamine
+                tase = self.skoor.points // 40
+                self.vastaste_kiirus = 9 + tase * 2
+                self.auto.kiirus = 7 + tase
+                        
                 self.auto.liiguta()
                 self.tekita_vastane()
 
                 for v_andmed in self.vastased[:]:
                     rect = v_andmed[0]
-                    rect.y += 10
+                    rect.y += self.vastaste_kiirus
+                            
                     if rect.y > self.KORGUS:
                         self.vastased.remove(v_andmed)
-                
+                        
                     if self.auto.rect.colliderect(rect):
                         self.mang_kaib = False
                         self.mang_labi = True
-                
+            
                 if self.teksti_timer > 0:
                     self.teksti_timer -= 1
 
